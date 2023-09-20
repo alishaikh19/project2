d22eadc201237d11887267af7f1acf3566ac4b52
Ali <shaikh.mdali257@gmail.com>
2023-09-20 22:47:42 +0530

    ff

--- dags/get_data_from_mongo.py
+++ dags/get_data_from_mongo.py
@@ -20,7 +20,7 @@ def get_data_from_mongo():
             'Clicked on Ad':'clicked'}, inplace=True)
 
    df.to_csv('/opt/airflow/dags/data/advertising1.csv', index=False)
-   print("this will load from mongodb avd")
+   print("this will load from mongodb avd 2")
 get_data_from_mongo()
 
 
