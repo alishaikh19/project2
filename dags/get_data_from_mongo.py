a4c5ae21cc0fada40f797d9f58c9cf9a67b3e2ce
Ali <shaikh.mdali257@gmail.com>
2023-09-20 20:29:24 +0530

    india

--- dags/get_data_from_mongo.py
+++ dags/get_data_from_mongo.py
@@ -1,5 +1,23 @@
 def get_data_from_mongo():
-    print("hello!")
-    print('bye')
-    print('see you')
+   import pymongo
+   import pandas as pd
+   client=pymongo.MongoClient("mongodb+srv://shaikhmdali257:6QNQvKj0R4vHgEk4@cluster0.bl5rcx8.mongodb.net/?retryWrites=true&w=majority")
+   db=client["mongodb"]
+   collection=db["Advertisement"]
+
+   data=[]
+   for i in collection.find():
+      data.append(i)
+
+   df=pd.DataFrame(data)
+   
+   df.rename(columns={'_id':'id', 
+            'Daily Time Spent on Site':'daily_time_spent_on_site', 
+            'Area Income':'income', 
+            'Daily Internet Usage':'daily_internet_usage',
+            'Ad Topic Line':'topic',
+            'Clicked on Ad':'clicked'}, inplace=True)
+   df.to_csv('/opt/dag/advertising1.csv',index=False)
+   # print(df)
+
 get_data_from_mongo()
--- dags/send_data_to_mysql.py
+++ dags/send_data_to_mysql.py
@@ -1,3 +1,34 @@
 def send_data_to_mysql():
-    print("hello!")
+    import pymysql
+    import csv
+
+
+    input_path='/opt/airflow/dag/data/advertising1.csv'
+
+    connection=pymysql.connect(host="mysql",user="root",password="password",db="mysql")
+    
+    
+    with connection.cursor() as cursor:
+        query="""create table advertising if not exists ((id varchar(200),
+                                                                daily_time_spent varchar(200), 
+                                                                age int, 
+                                                                area_income decimal(10,2),
+                                                                daily_internet_usage decimal(10,2),
+                                                                topic varchar(200),
+                                                                city varchar(200),
+                                                                male int,
+                                                                country varchar(200),
+                                                                timestamp timestamp,
+                                                                clicked int);"""
+        cursor.execute(query)
+
+
+        with open(input_path,"r") as csv_file:
+            csv_data=csv.reader(csv_file)
+            next(csv_data)
+            for i in csv_data:
+                insert_query="insert into advertising values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+                cursor.execute(insert_query,i)
+    connection.commit()
+    connection.close()
 send_data_to_mysql()
